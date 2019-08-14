7309d7d2b253e46c923373c71b5f70c4d1428a96
Giannina <giannecchini.a@gmail.com>
2019-08-14 13:53:48 -0400

    add nps component

--- src/components/Headerstrip/Headerstrip.js
+++ src/components/Headerstrip/Headerstrip.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import { Transition } from 'react-spring'
+import TopBarProgress from 'react-topbar-progress-indicator'
+import NpsRanking from '../NpsRanking/NpsRanking'
 
 import css from './Headerstrip.css'
 
@@ -11,6 +13,8 @@ class Headerstrip extends Component {
   static propTypes = {
     className: PropTypes.string,
     id: PropTypes.string,
+    nps: PropTypes.object,
+    npsShow: PropTypes.bool,
     onAccept: PropTypes.func,
     onDismiss: PropTypes.func,
     onSnooze: PropTypes.func,
@@ -23,11 +27,21 @@ class Headerstrip extends Component {
   static defaultProps = {
     title: 'Placeholder title',
     showDismiss: true,
+    npsShow: false,
     showSnooze: true,
     texts: {
       accept: 'Accept',
       dismiss: 'Dismiss',
       remind_me_later: 'Snooze',
+    },
+    nps: {
+      show: true,
+      texts: {
+        ranking: 10,
+        left: 'Poco probable',
+        right: 'Muy probable',
+      },
+      progressColor: 'rgba(230,123,47,1)',
     },
   }
 
@@ -35,7 +49,15 @@ class Headerstrip extends Component {
     super(props)
     this.state = {
       status: localStorage.getItem(this.statusKey()),
+      showNpsAccept: false,
     }
+    TopBarProgress.config({
+      barColors: {
+        '0': props.nps.progressColor,
+        '1.0': props.nps.progressColor,
+      },
+      shadowBlur: 2,
+    })
   }
 
   statusKey() {
@@ -52,6 +74,20 @@ class Headerstrip extends Component {
       this.props.onAccept()
     }
     this.setStorage('accepted')
+  }
+
+  onAcceptNps = (event, index) => {
+    event.preventDefault()
+    if (typeof this.props.onAccept === 'function') {
+      this.props.onAccept(index)
+    }
+    this.setState({ showNpsAccept: true }, () => {
+      localStorage.setItem(this.statusKey(), 'accepted')
+      localStorage.setItem(this.actionKey(), moment().format('YYYY-MM-DD'))
+      setTimeout(() => {
+        this.setState({ status: 'accepted' })
+      }, 4000)
+    })
   }
 
   onDismiss = event => {
@@ -100,48 +136,95 @@ class Headerstrip extends Component {
   }
 
   render() {
-    const { className, title, texts, id, showDismiss, showSnooze } = this.props
+    const {
+      className,
+      title,
+      texts,
+      id,
+      showDismiss,
+      npsShow,
+      showSnooze,
+      nps,
+    } = this.props
+    const { showNpsAccept } = this.state
     const shouldHide = !id || !this.shouldDisplay()
-
     const HeaderstripBar = (
       <div className={classNames(css.headerstrip, className)}>
-        <div className={classNames(css['headerstrip-title'])}>{title}</div>
-        <div className={classNames(css['headerstrip-options'])}>
-          {showDismiss && (
-            <div
-              className={classNames(css['headerstrip-option'])}
-              role="button"
-              tabIndex={0}
-              onClick={this.onDismiss}
-              onKeyPress={this.onDismiss}
-            >
-              {texts.dismiss || 'Dismiss'}
-            </div>
-          )}
-          {showSnooze && (
-            <div
-              className={classNames(css['headerstrip-option'])}
-              role="button"
-              tabIndex={0}
-              onClick={this.onSnooze}
-              onKeyPress={this.onSnooze}
-            >
-              {texts.remind_me_later || 'Remind me later'}
-            </div>
-          )}
-          <div
-            className={classNames(
-              css['headerstrip-option'],
-              css['headerstrip-rounded-option']
+        {!showNpsAccept ? (
+          <Fragment>
+            {npsShow && (
+              <NpsRanking
+                npsTexts={nps.texts}
+                callback={this.onAcceptNps}
+                title={title}
+              />
             )}
-            role="button"
-            tabIndex={0}
-            onClick={this.onAccept}
-            onKeyPress={this.onAccept}
-          >
-            {texts.accept || 'Accept'}
-          </div>
-        </div>
+            {!npsShow && (
+              <div
+                className={classNames(
+                  css[`headerstrip-title${npsShow && '-nps'}`]
+                )}
+              >
+                {title}
+              </div>
+            )}
+            <div className={classNames(css['headerstrip-options'])}>
+              {showDismiss && (
+                <div
+                  className={classNames(
+                    css['headerstrip-option'],
+                    css[`${npsShow && 'headerstrip-title-nps-options'}`]
+                  )}
+                  role="button"
+                  tabIndex={0}
+                  onClick={this.onDismiss}
+                  onKeyPress={this.onDismiss}
+                >
+                  {texts.dismiss || 'Dismiss'}
+                </div>
+              )}
+              {showSnooze && (
+                <div
+                  className={classNames(
+                    css['headerstrip-option'],
+                    css[`${npsShow && 'headerstrip-title-nps-options'}`]
+                  )}
+                  role="button"
+                  tabIndex={0}
+                  onClick={this.onSnooze}
+                  onKeyPress={this.onSnooze}
+                >
+                  {texts.remind_me_later || 'Remind me later'}
+                </div>
+              )}
+              {!npsShow && (
+                <div
+                  className={classNames(
+                    css['headerstrip-option'],
+                    css['headerstrip-rounded-option']
+                  )}
+                  role="button"
+                  tabIndex={0}
+                  onClick={this.onAccept}
+                  onKeyPress={this.onAccept}
+                >
+                  {texts.accept || 'Accept'}
+                </div>
+              )}
+            </div>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <TopBarProgress />
+            <div
+              className={classNames(
+                css[`headerstrip-title${npsShow && '-nps'}`]
+              )}
+            >
+              {texts.accept || 'Accept'}
+            </div>
+          </Fragment>
+        )}
       </div>
     )
 
---
+++ src/components/NpsRanking/NpsRanking.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+
+import css from '../Headerstrip/Headerstrip.css'
+
+export default class NpsRanking extends React.PureComponent {
+  static propTypes = {
+    callback: PropTypes.func,
+    npsTexts: PropTypes.object,
+    title: PropTypes.string,
+  }
+
+  static defaultProps = {
+    npsTexts: {
+      ranking: 10,
+      left: 'Poco probable',
+      right: 'Muy probable',
+    },
+  }
+
+  render() {
+    const { npsTexts, callback, title } = this.props
+    const data = Array.from(Array(npsTexts.ranking), (x, index) => index + 1)
+    return (
+      <div className={classNames(css['headerstrip-nps-content'])}>
+        <div className={classNames(css[`headerstrip-title-nps'}`])}>
+          {title}
+        </div>
+        <div className={classNames(css['headerstrip-ranking'])}>
+          <div
+            style={{ marginRight: 2 }}
+            className={classNames(css['headerstrip-title-nps-options'])}
+          >
+            {npsTexts.left}
+          </div>
+          {data.map(index => (
+            <div
+              key={index}
+              role="button"
+              tabIndex="0"
+              className={classNames(css['headerstrip-ranking-cell'])}
+              onClick={event => {
+                callback(event, index)
+              }}
+              onKeyPress={event => {
+                callback(event, index)
+              }}
+            >
+              {index}
+            </div>
+          ))}
+          <div
+            style={{ marginLeft: 2 }}
+            className={classNames(css['headerstrip-title-nps-options'])}
+          >
+            {npsTexts.right}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
